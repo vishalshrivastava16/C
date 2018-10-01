bb72d619ffa958286b3ea94c9f4ec26320f69792
hasee <597482326@qq.com>
2018-10-01 17:39:00 +0800

    1

--- C++Programming/temp.cpp
+++ C++Programming/temp.cpp
@@ -1,14 +1,35 @@
 #include<iostream>
-#include<cmath>
 using namespace std;
+
+struct SongList{
+    char song[50];
+    char singer[20];
+    int point;
+};
+
 int main(){
-    int len=0,i=0;
-    char str[100];
-    cin.getline(str,100);
-    while(str[i++]!='\0'){
-        len++;
+    SongList List[5],tmp;
+    int i,j;
+    //  ‰»Î
+    for(i=0;i<5;i++)
+        cin>>List[i].song>>List[i].singer>>List[i].point;
+
+    // ≈≈–Ú
+    for(i=0;i<5;i++){
+        for(j=i+1;j<5;j++){
+            if(List[i].point<List[j].point){
+                tmp=List[i];
+                List[i]=List[j];
+                List[j]=tmp;
+            }
+        }
     }
-    cout<<len;
+
+    //  ‰≥ˆ
+    for(i=0;i<5;i++)
+        cout<<List[i].song<<' '<<List[i].singer<<' '<<List[i].point<<endl;
+
+
     system("pause");
 	return 0;
 }
---
+++ tmp.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    char point[4];
+    cout<<sizeof(point)<<endl;
+    cout<<sizeof(int)<<endl;
+    cout<<sizeof(long)<<endl;
+    system("pause");
+    return 0;
+}
